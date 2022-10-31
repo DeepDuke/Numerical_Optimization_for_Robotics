1b2ad322e596aa63a07d9ae1b3d122400b322dfe
deepduke <runningwater.jc@gmail.com>
2022-10-31 10:34:25 +0800

    add code framework

---
+++ Homework2/src/curve_generator/src/cubic_spline.hpp
@@ -0,0 +1,214 @@
+#pragma once
+
+#include "Eigen/Core"
+#include "cubic_curve.hpp"
+
+class CubicSpline
+{
+   public:
+    CubicSpline() = default;
+
+    ~CubicSpline() = default;
+
+    void Initialize(const Eigen::Vector2d& head_point, const Eigen::Vector2d& tail_point, int pieces_num)
+    {
+        head_point_ = head_point;
+        tail_point_ = tail_point;
+        pieces_num_ = pieces_num;
+
+        // Init matrix A and inverse of A
+        inner_points_num_ = pieces_num_ - 1;
+        A_.resize(inner_points_num_, inner_points_num_);
+        inv_A_.resize(inner_points_num_, inner_points_num_);
+        b_.resize(inner_points_num_, 2);
+
+        // Set values for matrix A_
+        size_t bandwidth = 1;
+        for (size_t i = 0; i < inner_points_num_; ++i)
+        {
+            for (size_t j = 0; j < inner_points_num_; ++j)
+            {
+                if (j < i - bandwidth || j > i + bandwidth)
+                {
+                    A_(i, j) = 0.0;
+                }
+                else if (i == j)
+                {
+                    A_(i, j) = 4.0;
+                }
+                else
+                {
+                    // j == i - bandwidth or j == i + bandwidth
+                    A_(i, j) = 1.0;
+                }
+            }
+        }
+
+        // Get the inverse matrix of A_
+        inv_A_ = A_.inverse();
+
+        // Init b_
+        b_.setZero();
+    }
+
+    void Update(const Eigen::Matrix2Xd& inner_points)
+    {
+        UpdateInnerPoints(inner_points);
+
+        UpdateMatrixB(inner_points);
+
+        UpdateCubicCurve(inner_points);
+    }
+
+    double GetStretchEnergy() const
+    {
+        // Must be called after Update() function is called
+        double energy = 0.0;
+        for(size_t i = 0; i < cubic_curve_.getPieceNum(); ++i)
+        {
+            const auto& piece = cubic_curve_[i];
+            const auto& coef_mat = piece.getCoeffMat();
+            auto ci = coef_mat.col(2);
+            auto di = coef_mat.col(3);
+            // 12di^T * di + 4ci^T * ci + 12ci^T*di
+            energy += 12 * di.squaredNorm() + 4 * ci.squaredNorm() + 12 * (ci(0) * di(0) + ci(1) * di(1));
+        }
+        return energy;
+    }
+
+    const Eigen::Matrix2Xd& GetGradients()
+    {
+        // TODO calc grad
+        return inner_points_gradients_;
+    }
+
+   private:
+    void UpdateInnerPoints(const Eigen::Matrix2Xd& inner_points)
+    {
+        if (inner_points.cols() != inner_points_num_)
+        {
+            ROS_ERROR("Size of inner_points not equal the initialized size!");
+            return;
+        }
+
+        // Update inner_points
+        inner_points_ = inner_points;
+    }
+
+    void UpdateMatrixB(const Eigen::Matrix2Xd& inner_points)
+    {
+        // Update b_, index of points is in 0 ~ n, 0 for head point, n for tail point, 1 ~ n-1 for inner points x1 ~
+        // x_{n-1} D0 = Dn = (0, 0), D1 ~ D_{n-1} saved in b_
+
+        for (size_t i = 0; i < inner_points_num_; ++i)
+        {
+            if (i == 0)
+            {
+                // D1 = 3*(x2 - x0)
+                b_(i, 0) = 3 * (inner_points(0, 1) - 0);
+                b_(i, 1) = 3 * (inner_points(1, 1) - 0);
+            }
+            else if (i < inner_points_num_ - 1)
+            {
+                // For Di = D2 ~ D_{n-2}, Di = 3*(x{i+2} - xi)
+                size_t idx1 = i - 1;
+                size_t idx2 = (i + 2) - 1;
+                auto col1 = inner_points.col(idx1);
+                auto col2 = inner_points.col(idx2);
+                b_(i, 0) = 3 * (col2(0) - col1(0));
+                b_(i, 1) = 3 * (col2(1) - col1(1));
+            }
+            else
+            {
+                // D_{n-1} = 3*(xn - x_{n-2}), i == inner_points_num_ - 1
+                auto col = inner_points.col(i - 1);  // x_{n-2}
+                b_(i, 0) = 3 * (0 - col(0));
+                b_(i, 1) = 3 * (0 - col(1));
+            }
+        }
+    }
+
+    void UpdateCubicCurve(const Eigen::Matrix2Xd& inner_points)
+    {
+        // Update coefficients for each piece of cubic_curve_
+        std::vector<Eigen::Matrix<double, 2, 4>> coef_vec;
+
+        // The first piece's coefficients
+        Eigen::Matrix<double, 2, 4> coef_mat0;
+        // a0 = x0
+        coef_mat0(0, 0) = head_point_(0);
+        coef_mat0(1, 0) = head_point_(1);
+        // b0 = D0
+        coef_mat0(0, 1) = 0;
+        coef_mat0(1, 1) = 0;
+        // c0 = 3*(x1 - x0) - 2*D0 - D1
+        coef_mat0(0, 2) = 3 * (inner_points_(0, 0) - head_point_(0)) - 2 * 0 - b_(0, 0);
+        coef_mat0(1, 2) = 3 * (inner_points_(1, 0) - head_point_(1)) - 2 * 0 - b_(0, 1);
+        // d0 = 2*(x0 - x1) + D0 + D1
+        coef_mat0(0, 3) = 2 * (head_point_(0) - inner_points_(0, 0)) + 0 + b_(0, 0);
+        coef_mat0(1, 3) = 2 * (head_point_(1) - inner_points_(1, 0)) + 0 + b_(0, 1);
+        coef_vec.emplace_back(coef_mat0);
+
+        // middle pieces
+        for (size_t i = 1; i < pieces_num_ - 1; ++i)
+        {
+            Eigen::Matrix<double, 2, 4> coef_mat;
+
+            auto xi = inner_points_.col(i - 1);
+            auto xi_next = inner_points.col(i);
+            auto Di = b_.row(i - 1);
+            auto Di_next = b_.row(i);
+            // a_i = x_i
+            coef_mat(0, 0) = xi(0);
+            coef_mat(1, 0) = xi(1);
+            // b_i = D_i
+            coef_mat(0, 1) = Di(0);
+            coef_mat(1, 1) = Di(1);
+            // c_i = 3*(x_{i+1} - x_i) - 2*D_i - D_{i+1}
+            coef_mat(0, 2) = 3 * (xi_next(0) - xi(0)) - 2 * Di(0) - Di_next(0);
+            coef_mat(1, 2) = 3 * (xi_next(1) - xi(1)) - 2 * Di(1) - Di_next(1);
+            // d_i = 2*(x_i - x_{i+1}) + D_i + D_{i+1}
+            coef_mat(0, 3) = 2 * (xi(0) - xi_next(0)) + Di(0) + Di_next(0);
+            coef_mat(1, 3) = 2 * (xi(1) - xi_next(1)) + Di(1) + Di_next(1);
+
+            coef_vec.emplace_back(coef_mat);
+        }
+
+        // Total points num (including start point and end point) is n+1, The last piece (start from x_{n-1} --> to end
+        // x_{n})
+        Eigen::Matrix<double, 2, 4> coef_mat_last;
+        // x_{n-1}
+        auto prev_xn = inner_points.col(inner_points_num_ - 1);
+        // D_{n-1}
+        auto prev_Dn = b_.row(inner_points_num_ - 1);
+        // a_{n-1} = x_{n-1}
+        coef_mat_last(0, 0) = prev_xn(0);
+        coef_mat_last(1, 0) = prev_xn(1);
+        // b_{n-1} = D_{n-1}
+        coef_mat_last(0, 1) = prev_Dn(0);
+        coef_mat_last(1, 1) = prev_Dn(1);
+        // c_{n-1} = 3*(x_n - x_{n-1}) - 2*D_{n-1} - D_n
+        coef_mat_last(0, 2) = 3 * (tail_point_(0) - prev_xn(0)) - 2 * prev_Dn(0) - 0;
+        coef_mat_last(1, 2) = 3 * (tail_point_(1) - prev_xn(1)) - 2 * prev_Dn(1) - 0;
+        // d_{n-1} = 2*(x_{n-1} - x_{n}) + D_{n-1} + D_{n}
+        coef_mat_last(0, 3) = 2 * (prev_xn(0) - tail_point_(0)) + prev_Dn(0) + 0;
+        coef_mat_last(1, 3) = 2 * (prev_xn(1) - tail_point_(1)) + prev_Dn(1) + 0;
+
+        coef_vec.emplace_back(coef_mat_last);
+
+        std::vector<double> duration(coef_vec.size(), 1.0);
+        cubic_curve_ = CubicCurve(duration, coef_vec);
+    }
+
+   private:
+    int pieces_num_;
+    size_t inner_points_num_;
+    Eigen::Vector2d head_point_;
+    Eigen::Vector2d tail_point_;
+    Eigen::Matrix2Xd inner_points_;
+    Eigen::Matrix2Xd inner_points_gradients_;
+    Eigen::MatrixXd A_;
+    Eigen::MatrixXd inv_A_;
+    Eigen::MatrixX2d b_;
+    CubicCurve cubic_curve_;
+};
---
+++ Homework2/src/curve_generator/src/path_smoother.hpp
@@ -0,0 +1,78 @@
+#pragma once
+
+#include "cubic_spline.hpp"
+#include "L-BFGS.hpp"
+
+#include <Eigen/Eigen>
+
+#include <cmath>
+#include <cfloat>
+#include <iostream>
+#include <vector>
+
+namespace path_smoother {
+
+class PathSmoother
+{
+   private:
+    CubicSpline cubic_spline;
+
+    int pieceN;
+    Eigen::Matrix3Xd diskObstacles;
+    double penaltyWeight;
+    Eigen::Vector2d headP;
+    Eigen::Vector2d tailP;
+    Eigen::Matrix2Xd points;
+    Eigen::Matrix2Xd gradByPoints;
+
+   private:
+    static inline double costFunction()
+    {
+        double cost = 0.0;
+        return cost;
+    }
+
+   public:
+    double GetPenaltyWeight() { return penaltyWeight; }
+
+    CubicSpline& GetCubicSpline()
+    {
+        return cubic_spline;
+    }
+
+    Eigen::Matrix3Xd GetDiskObstacle()
+    {
+        return diskObstacles;
+    }
+
+    inline bool setup(const Eigen::Vector2d &initialP,
+                      const Eigen::Vector2d &terminalP,
+                      const int &pieceNum,
+                      const Eigen::Matrix3Xd &diskObs,
+                      const double penaWeight)
+    {
+        pieceN = pieceNum;
+        diskObstacles = diskObs;
+        penaltyWeight = penaWeight;
+        headP = initialP;
+        tailP = terminalP;
+
+        cubic_spline.Initialize(initialP, terminalP, pieceNum);
+
+        points.resize(2, pieceN - 1);
+        gradByPoints.resize(2, pieceN - 1);
+
+        return true;
+    }
+
+    inline double optimize(CubicCurve &curve,
+                           const Eigen::Matrix2Xd &iniInPs,
+                           const double &relCostTol)
+    {
+        double min_cost = 0.0;
+        return min_cost;
+    }
+};
+
+
+} // namespace path_smoother
